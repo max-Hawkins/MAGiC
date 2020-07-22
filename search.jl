45cda374f9926f0c61c7595af9e90c8530209d23
max-Hawkins <email@example.com>
2020-07-21 21:38:16 -0500

    Working on Julia/CUDA complex data handling

--- search.jl
+++ search.jl
@@ -3,7 +3,6 @@ Guppi data processing algorithms for energy detection.
 """
 module search
     using Statistics
-    using BenchmarkTools
     using CUDA
     using Plots
     using Random
@@ -39,58 +38,38 @@ module search
     "Compute the power of complex voltage data and return the spectrogram.
         If sum_pols is true and the complex data is polarized, the polarized
         powers are summed."
-    function power_spec(complex_block, sum_pols=true)
-        power_block = abs2.(Array{Complex{Int16}}(complex_block))
+    function power_spec(complex_block, avg_dim=-1, use_gpu=true, return_gpu_data=false)
+        println("Calculating power spectrum")
 
-        if sum_pols && size(complex_block, 1) == 2
-            power_block = sum(power_block[:,:,:], dims=1)
+        if use_gpu # TODO: Check for GPU availability
+            println("Using GPU")
+            complex_block = CuArray{Complex{Int32}}(complex_block)
+            power_block   = CuArray{UInt16}(undef, size(complex_block))            
         end
+
+        power_block = abs2.(complex_block)
+
+        if avg_dim > 0 && avg_dim <= ndims(complex_block) && size(complex_block, avg_dim) > 1
+            println("Averaging along dim $avg_dim")
+            power_block = mean(power_block, dims=avg_dim)
+        end
+
+        if use_gpu && !return_gpu_data
+            power_block = Array(power_block)
+        elseif use_gpu && return_gpu_data
+            println("Returning GPU data")
+        end
+
         return power_block
     end
 
-    function plot_pol_complex(data)
-        pol_plot = plot(real(data[1,1,:]), labels="Pol 1 Real", ylims=(-1,7))
-            plot!(imag(data[1,1,:]), labels="Pol 1 Imag")
-
-            plot!(real(data[2,1,:]), labels="Pol 2 Real")
-            plot!(imag(data[2,1,:]), labels="Pol 2 imag")
-
-        display(pol_plot)
-        return pol_plot
+    
+    function kurtosis_dave(a::AbstractArray; dims=1)
+        m = mean(a, dims=dims)
+        aa = a .- m
+        k = mean(aa .^ 4 ) ./ (mean(aa .^ 2, dims=dims) .^ 2)
+        return k
     end
-
-    function kurtosis_complex(complex_block, bychan=false)
-        kurtosis_block = Array{Complex{Float32}}(undef, (size(complex_block, 1),1,size(complex_block, 3)))
-
-        u = mean(complex_block, dims=2)
-        s4_real = std(real(complex_block), dims=2) .^ 4
-        s4_imag = std(imag(complex_block), dims=2) .^ 4
-
-        for pol = 1:size(complex_block, 1)
-            println("Pol $pol")
-            for i = 1:size(complex_block, 3)
-                kurtosis_pol = Array{Complex{Float32}}(complex_block[pol,:,i])
-
-                for samp = 1:size(complex_block, 2)
-                    
-                    kurtosis_real = (real(kurtosis_pol[samp]) - u[pol,1,i]) ^ 4 / s4_real[pol,1,i]
-                    kurtosis_imag = (imag(kurtosis_pol[samp]) - u[pol,1,i]) ^ 4 / s4_imag[pol,1,i]
-
-                    kurtosis_pol[samp] = kurtosis_real + (kurtosis_imag)im
-                    
-                    # real(pol_complex_chan) =  map(x->(x .- real(u[pol,1,i])) .^ 4 /  s4_real[pol,1,i], real(pol_complex_chan))
-                    # imag(pol_complex_chan) =  map(x->(x .- imag(u[pol,1,i])) .^ 4 /  s4_imag[pol,1,i], imag(pol_complex_chan))
-            
-                    
-                end
-                kurtosis_block[pol,1,i] = mean(real(kurtosis_pol)) + (mean(imag(kurtosis_pol)))im
-                
-            end
-        end
-
-        return kurtosis_block
-    end
-
 
     "Calculate the kurtosis values for each coarse channel in a power spectrum.
         If by_chan is false, the statistics used when computing the kurtosis
@@ -101,21 +80,30 @@ module search
             println("Calculating kurtosis - using CPU and block statistics")
             u = mean(power_block)
             s4 = stdm(power_block, u) ^ 4
-            kurtosis_block = map(x->(x .- u) .^ 4 /  s4, power_block)
+            for pol = 1:size(power_block, 1)
+                println("Pol $pol")
+                for chan = 1:size(power_block, 3)
+                    for samp = 1:size(power_block, 2)
+                        kurtosis_block[pol, samp, chan] = (power_block[pol, samp, chan] - u) ^ 4 /  s4
+
+                    end
+                end
+            end
         else
             # TODO: had issues with mapping optimization, reverted to for loops. Check back later
             println("Calculating kurtosis by channel - using CPU")
+
+            u = mean(power_block, dims = 2)
+            s4 = std(power_block, dims = 2) .^ 4
+
             for pol = 1:size(power_block, 1)
-                
-                u = mean(power_block[pol,:,:], dims = 1)
-                s4 = std(power_block[pol,:,:], dims = 1) .^ 4
+                println("Pol $pol")
+                for chan = 1:size(power_block, 3)
+                    for samp = 1:size(power_block, 2)
+                        kurtosis_block[pol, samp, chan] = (power_block[pol, samp, chan] - u[pol,1,chan]) ^ 4 /  s4[pol,1,chan]
 
-                kurtosis_block[pol, :, :] = map(x->(x .- u) .^ 4 /  s4, power_block[pol, :, :])
-
-                # for i = 1:size(power_block, 3)
-                #     println("Pol $pol Chan $i")
-                #     kurtosis_block[pol, :, i] = map(x->(x .- u[i]) .^ 4 /  s4[i], power_block[pol, :, i])
-                # end
+                    end
+                end
             end
         end
         return kurtosis_block
@@ -133,14 +121,41 @@ module search
         return
     end
 
-
-    function power_spec_kernel(complex_block, power_block, nint)
-        start_i = ((blockIdx().x - 1) * blockDim().x + threadIdx().x) * nint
-
-        @cuprint("$(i)")
+    "Development function - do not use"
+    function get_test_data(fn::String)
+        raw, rh = search.load_guppi(fn)
+        complex = search.read_block_gr(raw, rh)
+        return complex
     end
 
-    function power_spec_gpu(h_complex_block, nint)
+
+    function power_spec_kernel(complex_block, power_block, nint, ntime)
+        pol = blockIdx().x
+        samp = (blockIdx().y - 1) * blockDim().x + threadIdx().x
+        chan = blockIdx().z
+        CUDA.real
+        
+        if samp > ntime
+            return
+        end
+        # @inbounds complex_val = complex_block[pol, samp, chan]
+        # @inbounds power_block[pol, samp, chan] = complex_val.re  #CUDA.real(complex_val) * CUDA.real(complex_val) + CUDA.imag(complex_val) * CUDA.imag(complex_val)
+
+        # start_i = ((blockIdx().x - 1) * blockDim().x + threadIdx().x) * nint
+        if pol == 1 && samp==1000 && chan == 30
+            
+            @cuprint("Start: (pol=$pol, samp=$samp, chan=$chan) = \t
+                    Complex: $(complex_block[pol, samp, chan]) Power: $(power_block[pol, samp, chan])\t
+                     Block = ($(blockIdx().x), $(blockIdx().y), $(blockIdx().z))\t
+                     Grid = ($(threadIdx().x), $(threadIdx().y), $(threadIdx().z)) $(blockDim().x)\n")
+        end
+        
+    
+
+        return nothing
+    end
+
+    function power_spec_gpu(h_complex_block, nint=1)
 	    npol, ntime, nchan  = size(h_complex_block)
         if npol == 1
             println("Complex data is not polarized. npols = $(npol)")
@@ -160,18 +175,25 @@ module search
         
         nthreads_per_chan = ntime / nint
 
-        block_dim = (ceil(nthreads_per_chan / MAX_THREADS_PER_BLOCK), nchan)
-        grid_dim  = (MAX_THREADS_PER_BLOCK)
+        block_dim = (npol, Int(ceil(nthreads_per_chan / MAX_THREADS_PER_BLOCK)), nchan)
+        grid_dim  = (MAX_THREADS_PER_BLOCK, 1, 1)
         println("block_dim: $(block_dim)  grid_dim: $(grid_dim)")
 
-        d_complex_block = CuArray{Int8}(h_complex_block)
+        # Split complex values to allow for calculation in kernel
+        # Julia kernels don't handle complex values well
+        h_complex_block_split = Array{Int8}(undef, (npol, ntime, nchan, 2))
+        h_complex_block_split[:,:,:,1] = real(h_complex_block)
+        h_complex_block_split[:,:,:,2] = imag(h_complex_block)
+        # println("h_complex_split: $(h_complex_block_split)")
 
-        h_power_block = zeros{Int16}(1, nthreads_per_chan, nchan)
+        d_complex_block = CuArray{Int8}(h_complex_block_split)
+
+        h_power_block = Array{UInt16}(undef, 1, Int(nthreads_per_chan), nchan)
         d_power_block = CuArray(h_power_block)
             
 
         CUDA.@sync begin
-            @cuda threads=block_dim blocks=grid_dim power_spec_kernel(d_complex_block, d_power_block, nint)
+            @cuda threads=grid_dim blocks=block_dim power_spec_kernel(d_complex_block, d_power_block, nint, ntime)
         end
         
         h_power_block = Array{Int16}(d_power_block)
@@ -230,6 +252,7 @@ module search
 
     end
 
+    "Create the ticks and labels for plots from GUPPI headers"
     function get_xticks(rh::GuppiRaw.Header, data_length, num_ticks=10)
         spec_flip = rh.obsbw < 0
         obsbw = spec_flip ? rh.obsbw : -1 * rh.obsbw
@@ -246,6 +269,12 @@ module search
     # Plot and display/save/return t
     function plot_1d(block_data, rh::GuppiRaw.Header, type::String, disp=true, save=false)
         gr()
+        show_legend = false
+        num_pols = size(block_data, 1)
+        if num_pols > 1
+            show_legend = true
+        end
+
         # Bandpass plotting
         if type=="b" || type=="bandpass"
             title_prefix = "Bandpass"
@@ -268,25 +297,60 @@ module search
         ticks, labels, xinvert = get_xticks(rh, size(block_data,3))
         println("xflip: $xinvert")
         
-        p = plot(block_data[1,1,:],
-                title="$title_prefix of $(rh.basename) - Block $(rh.curblock)",
-                legend=false,
-                xlabel="Frequency (MHz)",
-                ylabel=ylab,
-                xticks=(ticks, labels),
-                xflip=xinvert)
-
-        println("p: $p)")
+        p = plot()
+        for pol = 1:num_pols
+            plot!(p, block_data[pol,1,:],
+                    title="$title_prefix of $(rh.src_name) - Single Block",
+                    legend=show_legend,
+                    label = "Pol $pol",
+                    xlabel="Frequency (MHz)",
+                    ylabel=ylab,
+                    xticks=(ticks, labels),
+                    xflip=xinvert)
+        end
 
         if disp
             println("Displaying plot")
             display(p)
         end
         if save
-            save_fn = "$(save_prefix)_$(rh.basename)_block$(lpad(rh.curblock, 3, '0')).png"
+            save_fn = "$(save_prefix)_$(rh.src_name)_block$(lpad(rh.curblock, 3, '0')).png"
             println("Saveing plot to '$(save_fn)'")
             savefig(p,save_fn)
         end
         return p
+    end
+
+    "Calculate the kurtosis of complex samples"
+    function kurtosis_complex(complex_block, bychan=false)
+        kurtosis_block = Array{Complex{Float32}}(undef, (size(complex_block, 1),1,size(complex_block, 3)))
+
+        u = mean(complex_block, dims=2)
+        s4_real = std(real(complex_block), dims=2) .^ 4
+        s4_imag = std(imag(complex_block), dims=2) .^ 4
+
+        for pol = 1:size(complex_block, 1)
+            println("Pol $pol")
+            for i = 1:size(complex_block, 3)
+                kurtosis_pol = Array{Complex{Float32}}(complex_block[pol,:,i])
+
+                for samp = 1:size(complex_block, 2)
+                    
+                    kurtosis_real = (real(kurtosis_pol[samp]) - u[pol,1,i]) ^ 4 / s4_real[pol,1,i]
+                    kurtosis_imag = (imag(kurtosis_pol[samp]) - u[pol,1,i]) ^ 4 / s4_imag[pol,1,i]
+
+                    kurtosis_pol[samp] = kurtosis_real + (kurtosis_imag)im
+                    
+                    # real(pol_complex_chan) =  map(x->(x .- real(u[pol,1,i])) .^ 4 /  s4_real[pol,1,i], real(pol_complex_chan))
+                    # imag(pol_complex_chan) =  map(x->(x .- imag(u[pol,1,i])) .^ 4 /  s4_imag[pol,1,i], imag(pol_complex_chan))
+            
+                    
+                end
+                kurtosis_block[pol,1,i] = mean(real(kurtosis_pol)) + (mean(imag(kurtosis_pol)))im
+                
+            end
+        end
+
+        return kurtosis_block
     end
 end
