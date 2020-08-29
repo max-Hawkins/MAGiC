9d7160252b9869c957c2a47e52a28aacbc9bff75
Max Hawkins <mhawkins@blc03.bl.pvt>
2020-08-29 18:10:12 +0000

    Impement command line arguments in julia calc thread

--- hashpipe_null_thread_test.jl
+++ hashpipe_null_thread_test.jl
@@ -1,4 +1,5 @@
 include("./hashpipe.jl")
+using ArgParse
 # HPGUPPI_DATABUF.h constants
 const ALIGNMENT_SIZE = 4096
 const N_INPUT_BLOCKS = 24
@@ -6,6 +7,7 @@ const BLOCK_HDR_SIZE = 5*80*512
 const BLOCK_DATA_SIZE = 128*1024*1024
 const padding_size = ALIGNMENT_SIZE - (sizeof(hashpipe_databuf_t)%ALIGNMENT_SIZE)
 const BLOCK_SIZE = BLOCK_HDR_SIZE + BLOCK_DATA_SIZE
+
 # typedef struct hpguppi_input_block {
 #     char hdr[BLOCK_HDR_SIZE];
 #     char data[BLOCK_DATA_SIZE];
@@ -34,49 +36,86 @@ mutable struct hpguppi_input_databuf_t
     p_blocks::Ptr{Any}
 end
 
-instance_id = 0
-input_db_id = 2
-cur_block_in = 0
-cur_block_out = 0
-status_key = "GPUSTAT"  
-status = hashpipe_status_t(0,0,0,0)
-r_status = Ref(status)
+function parse_commandline()
+	s = ArgParseSettings()
 
-hashpipe_status_attach(instance_id, r_status)
-display(status)
-
-input_db = hashpipe_databuf_attach(instance_id, input_db_id)
-println(status.p_buf)
-time_per_block = 1
-
-while true
-	tick = time_ns()
-
-    hashpipe_status_buf_lock_unlock(r_status) do 
-			hputi4(status.p_buf, Cstring(pointer("GPUBLKIN")), Cint(cur_block_in));
-			hputs(status.p_buf, status_key, "waiting");
-			hputi4(status.p_buf, Cstring(pointer("GPUBKOUT")), Cint(cur_block_out));
-			# hputi8(r_stat.p_buf,"GPUMCNT",mcnt);
+	@add_arg_table s begin
+		"--inst_id"
+			help = "hashpipe instance id to attach to"
+			arg_type = Int
+			default = 0
+		"--input_db"
+			help = "input hashpipe databuf to get raw data from"
+			arg_type = Int
+			default = 2
+		"--verbose"
+			help = "verbose output"
+			action = :store_true
 	end
-
-    while (rv=hashpipe_databuf_wait_filled(input_db, cur_block_in)) != HASHPIPE_OK
-        if rv==HASHPIPE_TIMEOUT
-            # println("GPU Timeout")
-        else
-            println("Error waiting for filled databuf")
-        end 
-        # TODO: Finish checking
-    end
-
-	hashpipe_status_buf_lock_unlock(r_status) do
-    	hputs(status.p_buf, status_key, "processing gpu");
-    	#hputs(status.p_buf, "T/BLKMS", time_per_block);
+		
+	args = parse_args(s)
+	println(args)
+	if args["verbose"]
+		println("Parsed args:")
+		for (arg,val) in args
+			println(" $arg => $val")
+		end
 	end
-
-    hashpipe_databuf_set_free(input_db, cur_block_in)
-    global cur_block_in = (cur_block_in + 1) % N_INPUT_BLOCKS
-	tock = time_ns()
-	global time_per_block = Int(tock - tick) / 1e6
-	print("Elapsed (ms): ",time_per_block) 
+	return args
 end
+
+function main()
+	args = parse_commandline()
+	
+	instance_id = args["inst_id"]
+	input_db_id = args["input_db"]
+	verbose     = args["verbose"]
+	
+	instance_id = 0
+	input_db_id = 2
+	cur_block_in = 0
+	cur_block_out = 0
+	status_key = "GPUSTAT"  
+	
+	status = hashpipe_status_t(0,0,0,0)
+	r_status = Ref(status)
+
+	hashpipe_status_attach(instance_id, r_status)
+	display(status)
+
+	input_db = hashpipe_databuf_attach(instance_id, input_db_id)
+	println(status.p_buf)
+	time_per_block = 1
+
+	while true
+		tick = time_ns()
+
+		hashpipe_status_buf_lock_unlock(r_status) do 
+				hputi4(status.p_buf, Cstring(pointer("GPUBLKIN")), Cint(cur_block_in));
+				hputs(status.p_buf, status_key, "waiting");
+				hputi4(status.p_buf, Cstring(pointer("GPUBKOUT")), Cint(cur_block_out));
+		end
+
+		while (rv=hashpipe_databuf_wait_filled(input_db, cur_block_in)) != HASHPIPE_OK
+			if rv==HASHPIPE_TIMEOUT
+				# println("GPU Timeout")
+			else
+				println("Error waiting for filled databuf")
+			end 
+			# TODO: Finish checking
+		end
+
+		hashpipe_status_buf_lock_unlock(r_status) do
+			hputs(status.p_buf, status_key, "processing gpu");
+			#hputs(status.p_buf, "T/BLKMS", time_per_block);
+		end
+
+		hashpipe_databuf_set_free(input_db, cur_block_in)
+		cur_block_in = (cur_block_in + 1) % N_INPUT_BLOCKS
+		tock = time_ns()
+		time_per_block = Int(tock - tick) / 1e6
+		print("Elapsed (ms): ",time_per_block) 
+	end
+end
+main()
 
